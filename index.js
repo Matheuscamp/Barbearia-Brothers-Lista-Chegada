ed61a8eb6f01e8ee68eed14ca8da8fe3b74b042f
Matheuscamp <matheus.camp32@gmail.com>
2023-03-21 12:26:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const onOpenModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "flex";
+};
+const onCloseModal = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+};
